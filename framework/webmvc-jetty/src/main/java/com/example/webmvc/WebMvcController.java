5d7ade7a1af191e95488166e62fc9ce273eeaa78
Sébastien Deleuze <sdeleuze@vmware.com>
2023-06-27 16:26:16 +0200

    Introduce Jetty sample

---
+++ framework/webmvc-jetty/src/main/java/com/example/webmvc/WebMvcController.java
@@ -0,0 +1,14 @@
+package com.example.webmvc;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebMvcController {
+
+	@GetMapping("/")
+	public String hello() {
+		return "Hello from Spring MVC and Jetty";
+	}
+
+}
