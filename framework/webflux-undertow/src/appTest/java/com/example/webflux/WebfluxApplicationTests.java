4c6fe33697b8685bd844533004290111c3d6b4ec
Sébastien Deleuze <sdeleuze@vmware.com>
2023-07-06 11:17:09 +0200

    Add WebFlux with Undertow tests

---
+++ framework/webflux-undertow/src/appTest/java/com/example/webflux/WebfluxApplicationTests.java
@@ -0,0 +1,35 @@
+package com.example.webflux;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.cr.smoketest.support.junit.ApplicationTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ApplicationTest
+class WebfluxApplicationTests {
+
+	@Test
+	void stringResponseBody(WebTestClient client) {
+		client.get()
+			.exchange()
+			.expectStatus()
+			.isOk()
+			.expectBody()
+			.consumeWith((result) -> assertThat(new String(result.getResponseBodyContent()))
+				.isEqualTo("Hello from Spring WebFlux and Undertow"));
+	}
+
+	@Test
+	void resourceInStatic(WebTestClient client) {
+		client.get()
+			.uri("foo.html")
+			.exchange()
+			.expectStatus()
+			.isOk()
+			.expectBody()
+			.consumeWith((result) -> assertThat(new String(result.getResponseBodyContent())).isEqualTo("Foo"));
+	}
+
+}
