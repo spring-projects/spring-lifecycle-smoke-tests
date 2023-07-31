7ab9f3178c46c5b95cc1e02d80eb3a4025fab87d
Sébastien Deleuze <sdeleuze@vmware.com>
2023-07-31 18:15:41 +0200

    Upgrade to Azul CraC JDK 17.0.8

--- ci/images/get-crac-jdk-url.sh
+++ ci/images/get-crac-jdk-url.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 
-echo "https://cdn.azul.com/zulu/bin/zulu17.42.21-ca-crac-jdk17.0.7-linux_x64.tar.gz"
+echo "https://cdn.azul.com/zulu/bin/zulu17.44.17-ca-crac-jdk17.0.8-linux_x64.tar.gz"
