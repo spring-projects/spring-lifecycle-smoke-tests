9138f2fc2e55a69010a7e5be40646d1dcc5e27c0
Stéphane Nicoll <stephane.nicoll@broadcom.com>
2024-07-16 15:25:19 +0200

    Add lifecycle smoke test ci plugin
    This commit adds a plugin that is heavily based on the one used by the
    AOT smoke tests. It uses the description of each lifecycle smoke tests
    to generate a GitHub Actions workflow with the necessary jobs.
    
    There is also an additional task that generates or update a status
    document that list the smoke tests per generation with a link to the
    code and workflow.
    
    See gh-61

---
+++ gradle/plugins/lifecycle-smoke-test-ci-plugin/src/main/java/org/springframework/lifecycle/gradle/GenerateGitHubActionsWorkflows.java
@@ -0,0 +1,143 @@
+/*
+ * Copyright 2022-2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.lifecycle.gradle;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import org.gradle.api.DefaultTask;
+import org.gradle.api.GradleException;
+import org.gradle.api.file.DirectoryProperty;
+import org.gradle.api.provider.ListProperty;
+import org.gradle.api.provider.Property;
+import org.gradle.api.tasks.Input;
+import org.gradle.api.tasks.OutputDirectory;
+import org.gradle.api.tasks.TaskAction;
+
+/**
+ * Task to generate the GitHub Actions workflows for the smoke tests.
+ *
+ * @author Andy Wilkinson
+ */
+public abstract class GenerateGitHubActionsWorkflows extends DefaultTask {
+
+	private static final String GITHUB_REPOSITORY = "spring-projects/spring-lifecycle-smoke-tests";
+
+	@OutputDirectory
+	public abstract DirectoryProperty getOutputDirectory();
+
+	@Input
+	public abstract ListProperty<SmokeTest> getSmokeTests();
+
+	@Input
+	public abstract Property<String> getGitBranch();
+
+	@Input
+	public abstract Property<String> getCronSchedule();
+
+	@Input
+	public abstract Property<String> getSpringBootGeneration();
+
+	public GenerateGitHubActionsWorkflows() {
+		getGitBranch().convention(getSpringBootGeneration());
+		getOutputDirectory().convention(getSpringBootGeneration()
+			.flatMap((generation) -> getProject().getLayout().getBuildDirectory().dir("workflows/" + generation)));
+	}
+
+	@TaskAction
+	void generateWorkflows() {
+		getProject().delete(getOutputDirectory());
+		getSmokeTests().get().forEach(this::generateWorkflow);
+	}
+
+	void generateWorkflow(SmokeTest smokeTest) {
+		File workflowFile = getOutputDirectory()
+			.file(getSpringBootGeneration().get() + "-" + smokeTest.group() + "-" + smokeTest.name() + ".yml")
+			.get()
+			.getAsFile();
+		workflowFile.getParentFile().mkdirs();
+		String workflowName = getSpringBootGeneration().get() + " | " + name(smokeTest.group()) + " Smoke Tests | "
+				+ name(smokeTest.name());
+		try (PrintWriter writer = new PrintWriter(new FileWriter(workflowFile))) {
+			writer.println("name: " + workflowName);
+			writer.println("on:");
+			writer.println("  schedule:");
+			writer.println("    - cron : '" + getCronSchedule().get() + "'");
+			writer.println("  workflow_dispatch:");
+			writer.println("jobs:");
+			if (smokeTest.tests()) {
+				writeJob(writer, smokeTest, "test");
+			}
+			if (smokeTest.appTests()) {
+				writeJob(writer, smokeTest, "appTest");
+				writeJob(writer, smokeTest, "checkpointRestoreAppTest");
+			}
+		}
+		catch (IOException ex) {
+			throw new GradleException("Failed to write workflow file '" + workflowFile + "'", ex);
+		}
+	}
+
+	private void writeJob(PrintWriter writer, SmokeTest smokeTest, String taskName) {
+		writer.println("  " + jobId(smokeTest.name(), taskName) + ":");
+		writer.println("    name: " + name(smokeTest.name() + " " + taskName));
+		writer.println("    uses: ./.github/workflows/smoke-test.yml");
+		writer.println("    secrets: inherit");
+		writer.println("    with:");
+		writer.println("      checkout_repository: " + GITHUB_REPOSITORY);
+		writer.println("      checkout_ref: " + getGitBranch().get());
+		writer.println("      project: " + smokeTest.group() + ":" + smokeTest.name());
+		writer.println("      task: " + taskName);
+	}
+
+	private String jobId(String smokeTestName, String taskName) {
+		StringBuilder output = new StringBuilder();
+		output.append(smokeTestName.replace("-", "_"));
+		output.append("_");
+		for (char c : taskName.toCharArray()) {
+			if (Character.isUpperCase(c)) {
+				output.append("_");
+				output.append(Character.toLowerCase(c));
+			}
+			else {
+				output.append(c);
+			}
+		}
+		return output.toString();
+	}
+
+	private String name(String input) {
+		StringBuilder output = new StringBuilder(input.length());
+		char previous = ' ';
+		for (char c : input.replace("-", " ").toCharArray()) {
+			if (previous == ' ') {
+				output.append(Character.toUpperCase(c));
+			}
+			else {
+				if (Character.isUpperCase(c)) {
+					output.append(' ');
+				}
+				output.append(c);
+			}
+			previous = c;
+		}
+		return output.toString();
+	}
+
+}
---
+++ gradle/plugins/lifecycle-smoke-test-ci-plugin/src/main/java/org/springframework/lifecycle/gradle/UpdateStatusPage.java
@@ -0,0 +1,154 @@
+/*
+ * Copyright 2022-2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.lifecycle.gradle;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.SortedSet;
+import java.util.TreeMap;
+import java.util.TreeSet;
+import java.util.function.Predicate;
+
+import org.gradle.api.DefaultTask;
+import org.gradle.api.file.RegularFileProperty;
+import org.gradle.api.provider.MapProperty;
+import org.gradle.api.tasks.Input;
+import org.gradle.api.tasks.OutputFile;
+import org.gradle.api.tasks.TaskAction;
+
+/**
+ * Task to update the {@code STATUS.adoc} with the smoke tests.
+ *
+ * @author Stephane Nicoll
+ */
+public abstract class UpdateStatusPage extends DefaultTask {
+
+	@OutputFile
+	public abstract RegularFileProperty getOutputFile();
+
+	@Input
+	public abstract MapProperty<String, List<SmokeTest>> getSmokeTests();
+
+	@TaskAction
+	void updateStatusPage() throws IOException {
+		List<String> lines = new ArrayList<>();
+		lines.add("= Smoke Tests Status");
+		lines.add(":toc:");
+		lines.add("");
+		lines.add("Check each test for potential configuration guidance.");
+		lines.add("");
+		getSmokeTests().get().forEach((name, tests) -> handleLocation(lines, name, tests));
+		Files.write(getOutputFile().get().getAsFile().toPath(), lines);
+	}
+
+	private void handleLocation(List<String> content, String generation, List<SmokeTest> smokeTests) {
+		content.add(":toc-title: %s Projects".formatted(generation));
+		content.add("== %s Projects".formatted(generation));
+		content.add("");
+		Map<String, SortedSet<SmokeTest>> groupedSmokeTests = new TreeMap<>();
+		for (SmokeTest smokeTest : smokeTests) {
+			groupedSmokeTests
+				.computeIfAbsent(smokeTest.group(), (group) -> new TreeSet<>(Comparator.comparing(SmokeTest::name)))
+				.add(smokeTest);
+		}
+		groupedSmokeTests.forEach((group, tests) -> {
+			content.add("=== " + capitalize(group));
+			content.add("");
+			content.add("[%header,cols=\"2\"]");
+			content.add("|===");
+			content.add("h|Smoke Test");
+			content.add("h|Status");
+			content.add("");
+			for (SmokeTest test : tests) {
+				String name = test.name();
+				String workflowUrl = workflowUrl(generation, name);
+				content.add("|" + testUrl(group, name) + "[" + name + "]");
+				content.add("| image:%s/badge.svg[\"Status\", link=\"%s\"]".formatted(workflowUrl, workflowUrl));
+				content.add("");
+			}
+			content.add("|===");
+			content.add("");
+		});
+	}
+
+	private String workflowUrl(String generation, String name) {
+		return "https://github.com/spring-projects/spring-lifecycle-smoke-tests/actions/workflows/%s-%s.yml"
+			.formatted(generation, name);
+	}
+
+	private String capitalize(String input) {
+		StringBuffer buffer = new StringBuffer(input.length());
+		for (char c : input.toCharArray()) {
+			buffer.append(buffer.isEmpty() ? Character.toUpperCase(c) : c);
+		}
+		return buffer.toString();
+	}
+
+	private String testUrl(String group, String name) {
+		return "https://github.com/spring-projects/spring-lifecycle-smoke-tests/tree/main/" + group + "/" + name;
+	}
+
+	private enum TestType {
+
+		APP_TEST(SmokeTest::appTests, "-app-test", "appTest"),
+		CR_APP_TEST(SmokeTest::appTests, "-cr-app-test", "checkpointRestoreAppTest"),
+		TEST(SmokeTest::tests, "-test", "test");
+
+		private final Predicate<SmokeTest> predicate;
+
+		private final String urlSuffix;
+
+		private final String taskName;
+
+		TestType(Predicate<SmokeTest> predicate, String suffix, String taskName) {
+			this.predicate = predicate;
+			this.urlSuffix = suffix;
+			this.taskName = taskName;
+		}
+
+		String badge(SmokeTest smokeTest) {
+			if (!this.predicate.test(smokeTest)) {
+				return "";
+			}
+			return "image:" + badgeUrl(smokeTest.name(), this.urlSuffix) + "[link="
+					+ jobUrl(smokeTest.name(), this.urlSuffix) + "]";
+		}
+
+		String taskName() {
+			return this.taskName;
+		}
+
+		private String badgeUrl(String name, String suffix) {
+			return workflowUrl(name) + "/badge.svg?branch=main";
+		}
+
+		private String jobUrl(String name, String suffix) {
+			return workflowUrl(name);
+		}
+
+		private String workflowUrl(String name) {
+			return "https://github.com/spring-projects/spring-lifecycle-smoke-tests/workflows/3.3.x-%s.yml"
+				.formatted(name);
+		}
+
+	}
+
+}
